5854ab9509374f2367ac425c33b9971490cb52c2
Josh Morman <mormjb@gmail.com>
2020-02-18 10:23:01 -0500

    pybind: gateway sync block

--- gnuradio-runtime/lib/block_gateway_impl.cc
+++ gnuradio-runtime/lib/block_gateway_impl.cc
@@ -55,13 +55,19 @@ block_gateway_impl::block_gateway_impl(const py::handle& p,
                                        gr::io_signature::sptr out_sig)
     : block(name, in_sig, out_sig)//, _py_handle(py_object)
 {
-    p.attr("test2")();
-    p.attr("test")();
     _py_handle = p;
 }
 
 void block_gateway_impl::forecast(int noutput_items, gr_vector_int& ninput_items_required)
 {
+    py::gil_scoped_acquire acquire;
+    // ninput_items_required[0] = noutput_items; // todo: replace with call back to python
+    py::object ret_ninput_items_required = _py_handle.attr("handle_forecast")(noutput_items, ninput_items_required.size());
+
+    gr_vector_int tmp_int_vector =  ret_ninput_items_required.cast<std::vector<int>>();
+    ninput_items_required = tmp_int_vector;
+
+    std::cout << noutput_items << "," << ninput_items_required[0] << std::endl;
 
 }
 
@@ -72,10 +78,41 @@ int block_gateway_impl::general_work(int noutput_items,
 {
     py::gil_scoped_acquire acquire;
     // py::scoped_interpreter guard{};
-    _py_handle.attr("test")();
-    _py_handle.attr("general_work")(noutput_items, ninput_items, input_items, output_items);
 
-    return 0;
+    // std::vector<uint64_t> input_item_ptrs, output_item_ptrs;
+
+    // for (const void *p : input_items)
+    // {
+    //     input_item_ptrs.push_back(uint64_t(p));
+    // }
+
+    // for (void *p : output_items)
+    // {
+    //     output_item_ptrs.push_back(uint64_t(p));
+    // }
+
+    // std::vector<py::buffer_info> in_buffers;
+    // int i=0;
+    // for (const void *b : input_items)
+    // {
+    //     in_buffers.push_back(
+    //         py::buffer_info(
+    //             b,                            /* Pointer to buffer */
+    //             sizeof(gr_complex),                       /* Size of one scalar */
+    //             std::string("gr_complex"), //py::format_descriptor<gr_complex>::format(), /* Python struct-style format descriptor */
+    //             1,                                   /* Number of dimensions */
+    //             { ninput_items[i], },              /* Buffer dimensions */
+    //             { sizeof(gr_complex), }
+    //         )
+    //     );
+    //     i++;
+    // }
+    
+    // _py_handle.attr("test")();
+    std::cout << "c++ ninput_items = " << ninput_items[0] << std::endl;
+    py::object ret = _py_handle.attr("general_work")(noutput_items, ninput_items, input_items, output_items);
+    
+    return ret.cast<int>();;
 }
 
 int block_gateway_impl::work(int noutput_items,
--- gnuradio-runtime/python/gnuradio/gr/gateway.py
+++ gnuradio-runtime/python/gnuradio/gr/gateway.py
@@ -24,17 +24,17 @@ from __future__ import unicode_literals
 
 
 import numpy
+import ctypes
 
 from . import gr_python as gr
 from .gr_python import io_signature  # , io_signaturev
 from .gr_python import block_gateway
 
+
 ########################################################################
 # Magic to turn pointers into numpy arrays
 # http://docs.scipy.org/doc/numpy/reference/arrays.interface.html
 ########################################################################
-
-
 def pointer_to_ndarray(addr, dtype, nitems):
     class array_like(object):
         __array_interface__ = {
@@ -113,20 +113,22 @@ class py_io_signature(object):
 class gateway_block(object):
 
     def __init__(self, name, in_sig, out_sig):
+        self._decim = 1
+        self._interp = 1
 
         # Normalize the many Python ways of saying 'nothing' to '()'
         in_sig = in_sig or ()
         out_sig = out_sig or ()
-
+        
         # Backward compatibility: array of type strings -> py_io_signature
         if type(in_sig) is py_io_signature:
-            self.__in_sig = in_sig
+            self.in_sig = in_sig
         else:
-            self.__in_sig = py_io_signature(len(in_sig), len(in_sig), in_sig)
+            self.in_sig = py_io_signature(len(in_sig), len(in_sig), in_sig)
         if type(out_sig) is py_io_signature:
-            self.__out_sig = out_sig
+            self.out_sig = out_sig
         else:
-            self.__out_sig = py_io_signature(
+            self.out_sig = py_io_signature(
                 len(out_sig), len(out_sig), out_sig)
 
         # self.__gateway = block_gateway(self, name, self.__in_sig, self.__out_sig)
@@ -138,13 +140,26 @@ class gateway_block(object):
         # pb_out_sig = gr.io_signature.makev(len(out_sig), len(out_sig), out_sig)
 
         self.__gateway = block_gateway(
-            self, name, self.__in_sig.gr_io_signature(), self.__out_sig.gr_io_signature())
+            self, name, self.in_sig.gr_io_signature(), self.out_sig.gr_io_signature())
 
     def to_basic_block(self):
         """
         Makes this block connectable by hier/top block python
         """
         return self.__gateway.to_basic_block()
+    
+    def fixed_rate_noutput_to_ninput(self, noutput_items):
+        return int((noutput_items * self._decim / self._interp) + self.__gateway.history() - 1)
+
+    def handle_forecast(self, noutput_items, ninputs):
+        """
+        This is the handler function for forecast calls from 
+        block_gateway in c++ across pybind11 wrappers
+        """
+        ninput_items_required = [0] * ninputs
+        self.forecast(noutput_items, ninput_items_required)
+        
+        return ninput_items_required
 
     def forecast(self, noutput_items, ninput_items_required):
         """
@@ -152,8 +167,8 @@ class gateway_block(object):
         this is the default implementation
         """
         for i in range(len(ninput_items_required)):
-            ninput_items_required[i] = noutput_items + self.history() - 1
-
+            ninput_items_required[i] = noutput_items + self.__gateway.history() - 1
+        
     def general_work(self, *args, **kwargs):
         """general work to be overloaded in a derived class"""
         raise NotImplementedError("general work not implemented")
@@ -179,6 +194,9 @@ class gateway_block(object):
 # Wrappers for the user to inherit from
 ########################################################################
 class sync_block(gateway_block):
+    _decim = 1
+    _interp = 1
+    
     """
     Args:
     name (str): block name
@@ -190,20 +208,38 @@ class sync_block(gateway_block):
     For backward compatibility, a sequence of numpy type names is also
     accepted as an io signature.
     """
-
     def __init__(self, name, in_sig, out_sig):
         gateway_block.__init__(self,
                                name=name,
                                in_sig=in_sig,
                                out_sig=out_sig
                                )
+        _decim = 1
+        _interp = 1
 
-    def general_work(self, noutput_items,
+    def handle_general_work(self, noutput_items, # todo: change to handle_general_work
                      ninput_items,
                      input_items,
                      output_items):
-        print("general_work")
-        
+                     
+        ninputs = len(input_items)
+        noutputs = len(output_items)
+        in_types = self.in_sig.port_types(ninputs)
+        out_types = self.out_sig.port_types(noutputs)
 
-    def test(self):
-        print("test")
+        ctypes.pythonapi.PyCapsule_GetPointer.restype = ctypes.c_void_p
+        ctypes.pythonapi.PyCapsule_GetPointer.argtypes = [ctypes.py_object, ctypes.c_char_p]
+
+        ii=[pointer_to_ndarray(
+            ctypes.pythonapi.PyCapsule_GetPointer(input_items[i],None),
+            in_types[i],
+            self.fixed_rate_noutput_to_ninput(noutput_items)
+        ) for i in range(ninputs)]
+
+        oo=[pointer_to_ndarray(
+            ctypes.pythonapi.PyCapsule_GetPointer(output_items[i],None),
+            out_types[i],
+            noutput_items
+        ) for i in range(noutputs)]           
+        return self.work(ii,oo)
+
