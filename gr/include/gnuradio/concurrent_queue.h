4fa79926abb449eb893bd2373812f9c7273827a2
Josh Morman <jmorman@gnuradio.org>
2022-08-11 14:47:46 -0400

    wip
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- gr/include/gnuradio/concurrent_queue.h
+++ gr/include/gnuradio/concurrent_queue.h
@@ -116,15 +116,79 @@ private:
     std::condition_variable _cond;
 };
 } // namespace gr
-#else
+#elif 0
 // atomic flag
-// described here: 
+// described here:
 // https://modernescpp.com/index.php/performancecomparison-of-condition-variables-and-atomics-in-c-20
 #include <atomic>
 #include <chrono>
 #include <deque>
 #include <iostream>
 #include <mutex>
+namespace gr {
+
+/**
+ * @brief Blocking Multi-producer Single-consumer Queue class
+ *
+ * @tparam T Data type of items in queue
+ */
+template <typename T>
+class concurrent_queue
+{
+public:
+    bool push(const T& msg)
+    {
+        std::unique_lock<std::mutex> l(_mutex);
+        _queue.push_back(msg);
+        l.unlock();
+        _cond.test_and_set();
+        _cond.notify_one();
+
+        return true;
+    }
+
+    // Non-blocking
+    bool try_pop(T& msg)
+    {
+        std::unique_lock<std::mutex> l(_mutex);
+        if (!_queue.empty()) {
+            msg = _queue.front();
+            _queue.pop_front();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    bool pop(T& msg)
+    {
+        _cond.wait(false);
+        std::unique_lock<std::mutex> l(_mutex);
+        msg = _queue.front();
+        _queue.pop_front();
+        l.unlock();
+        _cond.clear();
+        return true;
+    }
+    void clear()
+    {
+        std::unique_lock<std::mutex> l(_mutex);
+        _queue.clear();
+    }
+
+private:
+    std::deque<T> _queue;
+    std::mutex _mutex;
+    std::atomic_flag _cond{ true };
+};
+} // namespace gr
+#else
+#include <atomic>
+#include <chrono>
+#include <deque>
+#include <iostream>
+#include <mutex>
+using namespace std::chrono_literals;
 namespace gr {
 
 /**
@@ -163,11 +227,18 @@ public:
     }
     bool pop(T& msg)
     {
-        std::unique_lock<std::mutex> l(_mutex);
-        _cond.wait(false);
-        msg = _queue.front();
-        _queue.pop_front();
-        _cond.clear();
+        while (true) {
+            _cond.wait(false); // TODO - replace with a waitfor
+            std::unique_lock<std::mutex> l(_mutex);
+            if (_queue.empty()) {
+                continue;
+            }
+            msg = _queue.front();
+            _queue.pop_front();
+            if (_queue.empty())
+                _cond.clear();
+            break;
+        }
         return true;
     }
     void clear()
