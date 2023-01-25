3c8d45d0d87683fff98c2c45e14e22d401253566
Josh Morman <jmorman@gnuradio.org>
2023-01-25 10:52:12 -0500

    working bm_a
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- bench/sched/bm_a.cpp
+++ bench/sched/bm_a.cpp
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
     // app.add_option("-h,--help", "display help");
     app.add_option("--samples", samples, "Number of Samples");
     app.add_option("--stages", stages, "Number of copy blocks");
-    app.add_option("--buffer_size", stages, "Default Buffer Size");
+    app.add_option("--buffer_size", buffer_size, "Default Buffer Size");
     app.add_option("--nthreads", nthreads, "Number of threads (0: tpb)");
     app.add_option("--use_memcpy", use_memcpy, "use memcpy for copy");
     app.add_flag("--rt_prio", rt_prio, "Enable Real-time priority");
@@ -49,7 +49,7 @@ int main(int argc, char* argv[])
         auto head =
             streamops::head::make_cpu({ samples, sizeof(float)  });
         auto snk = blocks::null_sink::make({ 1, sizeof(float)  });
-        std::vector<gr::bench::copy::sptr> copy_blks(stages*3);
+        std::vector<gr::bench::copy<float>::sptr> copy_blks(stages*3);
         for (unsigned int i = 0; i < stages; i++) {
             copy_blks[i*3] = bench::copy<float>::make({ use_memcpy });
             copy_blks[i*3+1] = bench::copy<float>::make({ use_memcpy});
@@ -57,23 +57,27 @@ int main(int argc, char* argv[])
         }
         auto fg = flowgraph::make();
 
-        fg->connect(src, 0, head, 0);
 
-        gr::node_sptr last_node = head;       
+
+        gr::node_sptr last_node = src;       
 
         for (unsigned int i = 0; i < stages; i++) {
             fg->connect(last_node, 0, copy_blks[3*i], 0)->set_custom_buffer(
-                BUFFER_CPU_VMCIRC_ARGS->set_max_buffer_read(128);
+                BUFFER_CPU_VMCIRC_ARGS->set_max_buffer_read(128)
             );
             fg->connect(copy_blks[3*i], 0, copy_blks[3*i+1], 0)->set_custom_buffer(
-                BUFFER_CPU_VMCIRC_ARGS->set_max_buffer_read(1024);
+                BUFFER_CPU_VMCIRC_ARGS->set_max_buffer_read(1024)
             );
             fg->connect(copy_blks[3*i+1], 0, copy_blks[3*i+2], 0)->set_custom_buffer(
-                BUFFER_CPU_VMCIRC_ARGS->set_max_buffer_read(128)->set_min_buffer_read(32);
+                BUFFER_CPU_VMCIRC_ARGS->set_max_buffer_read(128)->set_min_buffer_read(32)
             );
+            // fg->connect(last_node, 0, copy_blks[3*i], 0);
+            // fg->connect(copy_blks[3*i], 0, copy_blks[3*i+1], 0);
+            // fg->connect(copy_blks[3*i+1], 0, copy_blks[3*i+2], 0);
             last_node = copy_blks[3*i+2];
         }
-        fg->connect(last_node, 0, snk, 0);
+        fg->connect(last_node, 0, head, 0);
+        fg->connect(head, 0, snk, 0);
 
         auto opts = schedulers::scheduler_nbt_options::make();
         opts->default_buffer_size = buffer_size;
@@ -84,18 +88,19 @@ int main(int argc, char* argv[])
             std::vector<block_sptr> block_group;
 
             block_group.push_back(src);
-            block_group.push_back(head);
 
 
-            for (int j = 0; j < stages; j++) {
+
+            for (size_t j = 0; j < stages * 3; j++) {
                 block_group.push_back(copy_blks[j]);
             }
 
+            block_group.push_back(head);
             block_group.push_back(snk);
 
-            sched->add_block_group(block_group);
+            // sched->add_block_group(block_group);
 
-            sched->add_block_group(block_group, "group" + std::to_string(i));
+            sched->add_block_group(block_group, "single thread group");
         }
 
         auto rt = runtime::make();
